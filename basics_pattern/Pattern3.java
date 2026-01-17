691e769a2ecb020eb4a769edd9cef10491187e78
Arpita Raj <singh.arpitaraj2001@gmail.com>
2026-01-17 17:16:59 +0530

    single row pattern

---
+++ basics_pattern/Pattern3.java
@@ -0,0 +1,10 @@
+public class Pattern3 {
+    static void RowPattern(int n){
+        for(int i=0; i<n; i++){
+            System.out.print("*");
+        }
+    }
+    public static void main(String[] args) {
+        RowPattern(5);
+    }
+}
