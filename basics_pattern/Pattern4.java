4a1a1abc08fd1955b26591c57c6c83dea366ed71
Arpita Raj <singh.arpitaraj2001@gmail.com>
2026-01-17 17:36:06 +0530

    Left right angle triangle pattern

---
+++ basics_pattern/Pattern4.java
@@ -0,0 +1,13 @@
+public class Pattern4 {
+    static void LeftAngleTrianglePattern(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        LeftAngleTrianglePattern(5);
+    }
+}
