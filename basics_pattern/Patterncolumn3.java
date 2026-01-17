3e4703a5521c851114a7e38313c704813dfe82aa
Arpita Raj <singh.arpitaraj2001@gmail.com>
2026-01-17 17:19:10 +0530

    single column pattern

---
+++ basics_pattern/Patterncolumn3.java
@@ -0,0 +1,10 @@
+public class Patterncolumn3{
+    static void PatternColumn(int n) {
+        for(int i=0; i<n; i++){
+            System.out.println("*");
+        }
+    }
+    public static void main(String[] args) {
+        PatternColumn(5);
+    }
+}
