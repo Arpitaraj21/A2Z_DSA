ff5990ef9f7630df4aa733d922f024c4e2e87c76
arpita0212 <arpita@pipra.solutions>
2026-01-19 13:18:33 +0530

    Number increasing and decreasing pattern

---
+++ Pattern12.java
@@ -0,0 +1,25 @@
+
+public class Pattern12 {
+
+    static void NumberIncreasingDecreasingPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            // Print spaces
+            for (int j = n - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            // Print increasing numbers
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
+            }
+            // Print decreasing numbers
+            for (int j = i - 1; j >= 0; j--) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberIncreasingDecreasingPattern(5);
+    }
+}
