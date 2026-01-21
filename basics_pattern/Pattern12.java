f13e59620a22daa3cdbf698e674ede20f4899314
arpita0212 <arpita@pipra.solutions>
2026-01-21 11:29:33 +0530

    hollow rectangle pattern

--- basics_pattern/Pattern12.java
+++ basics_pattern/Pattern12.java
@@ -1,25 +1,20 @@
 
 public class Pattern12 {
 
-    static void NumberIncreasingDecreasingPattern(int n) {
+    static void hollowRectangle(int n) {
         for (int i = 0; i < n; i++) {
-            // Print spaces
-            for (int j = n - i; j > 1; j--) {
-                System.out.print(" ");
-            }
-            // Print increasing numbers
-            for (int j = 0; j <= i; j++) {
-                System.out.print(j + 1);
-            }
-            // Print decreasing numbers
-            for (int j = i - 1; j >= 0; j--) {
-                System.out.print(j + 1);
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        NumberIncreasingDecreasingPattern(5);
+        hollowRectangle(5);
     }
 }
