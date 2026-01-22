e9058b566a44204d2944ae8ff4dadb671854ea89
arpita0212 <arpita@pipra.solutions>
2026-01-22 11:47:58 +0530

    check if a number is odd or even

---
+++ Mathematical_problem/CheckOddEven.java
@@ -0,0 +1,16 @@
+
+public class CheckOddEven {
+
+    static void EvenOdd(int n) {
+        if (n % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        EvenOdd(5);
+    }
+
+}
